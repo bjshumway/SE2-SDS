85e5e8d388b30e64810dfdf3f6a600a1af63eb9d
collenirwin <collenirwin@gmail.com>
2016-09-18 23:44:47 -0400

    Add a tool for removing namespaces from .cs files

---
+++ Tools/StripNameSpace/StripNameSpace/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace StripNameSpace {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnStrip = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.txtNS = new System.Windows.Forms.TextBox();
+            this.lblNS = new System.Windows.Forms.Label();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // ofd
+            // 
+            this.ofd.Multiselect = true;
+            this.ofd.Title = "Files to Strip";
+            this.ofd.FileOk += new System.ComponentModel.CancelEventHandler(this.ofd_FileOk);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(487, 51);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnStrip
+            // 
+            this.btnStrip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStrip.Location = new System.Drawing.Point(487, 80);
+            this.btnStrip.Name = "btnStrip";
+            this.btnStrip.Size = new System.Drawing.Size(75, 23);
+            this.btnStrip.TabIndex = 2;
+            this.btnStrip.Text = "Strip";
+            this.btnStrip.UseVisualStyleBackColor = true;
+            this.btnStrip.Click += new System.EventHandler(this.btnStrip_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(469, 238);
+            this.lstFiles.TabIndex = 2;
+            this.lstFiles.TabStop = false;
+            // 
+            // txtNS
+            // 
+            this.txtNS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNS.Location = new System.Drawing.Point(487, 25);
+            this.txtNS.Name = "txtNS";
+            this.txtNS.Size = new System.Drawing.Size(75, 20);
+            this.txtNS.TabIndex = 0;
+            // 
+            // lblNS
+            // 
+            this.lblNS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNS.AutoSize = true;
+            this.lblNS.Location = new System.Drawing.Point(484, 9);
+            this.lblNS.Name = "lblNS";
+            this.lblNS.Size = new System.Drawing.Size(53, 13);
+            this.lblNS.TabIndex = 4;
+            this.lblNS.Text = "NS Name";
+            // 
+            // bgw
+            // 
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 261);
+            this.Controls.Add(this.lblNS);
+            this.Controls.Add(this.txtNS);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnStrip);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Strip NS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnStrip;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.TextBox txtNS;
+        private System.Windows.Forms.Label lblNS;
+        private System.ComponentModel.BackgroundWorker bgw;
+    }
+}
+
---
+++ Tools/StripNameSpace/StripNameSpace/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StripNameSpace {
+    public partial class Form1 : Form {
+
+        const string OMIT = "[SNS]DO NOT WRITE THIS";
+        string errors = "";
+        string nsName = "";
+
+        List<string> lst = new List<string>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void ofd_FileOk(object sender, CancelEventArgs e) {
+            foreach (string file in ofd.FileNames) {
+                lstFiles.Items.Add(file);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e) {
+            ofd.ShowDialog();
+        }
+
+        private void btnStrip_Click(object sender, EventArgs e) {
+            errors = "";
+            nsName = txtNS.Text;
+            btnOpen.Enabled  = false;
+            btnStrip.Enabled = false;
+
+            foreach (string s in lstFiles.Items) {
+                lst.Add(s);
+            }
+
+            bgw.RunWorkerAsync();
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e) {
+            foreach (string s in lst) {
+                try {
+                    string fileText = "";
+
+                    using (StreamReader r = File.OpenText(s)) {
+                        fileText = r.ReadToEnd(); // dump the file content into fileText
+                    }
+
+                    string[] lines = fileText.Replace("\r", "").Split('\n');
+
+                    bool foundNS = false;
+                    for (int x = 0; x < lines.Length; x++) {
+                        if (lines[x].Trim().StartsWith("namespace " + nsName)) {
+                            lines[x] = OMIT; // mark the NS line for omittance
+                            foundNS  = true;
+
+                            break;
+                        }
+                    }
+
+                    if (foundNS) { // we have found and omitted the NS
+                        for (int x = 0; x < lines.Length; x++) {
+                            if (lines[x].StartsWith("    ")) {
+                                lines[x] = lines[x].Remove(0, 4); // remove 4 leading spaces
+                            }
+                        }
+
+                        StringBuilder sb = new StringBuilder();
+
+                        foreach (string line in lines) {
+                            if (line != OMIT) {
+                                sb.Append(line);
+                                sb.Append("\r\n");
+                            }
+                        }
+
+                        fileText = sb.ToString().TrimEnd('\r', '\n');
+
+                        // remove last '}'
+                        int lastBracketIndex = fileText.LastIndexOf('}');
+                        fileText = fileText.Remove(lastBracketIndex, 1);
+
+                        using (StreamWriter w = File.CreateText(s)) {
+                            w.Write(fileText);
+                        }
+                    }
+
+                } catch {
+                    errors += s + "\r\n";
+                }
+            }
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if (errors != "") {
+                MessageBox.Show("Couldn't strip the following file(s):\r\n" + errors);
+            } else {
+                MessageBox.Show("All stripped");
+            }
+
+            btnOpen.Enabled  = true;
+            btnStrip.Enabled = true;
+            lstFiles.Items.Clear();
+            lst.Clear();
+        }
+    }
+}
