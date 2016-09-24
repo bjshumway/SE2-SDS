d8818d2eefa52e6d3ebb1255d51c3c7eb657967c
Ben Shumway <benshums@gmail.com>
2016-09-24 12:42:49 -0400

    putting latest updated up

---
+++ TB-RPG/Assets/Scripts/UI/CharacterCreationMenu.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CharacterCreationMenu : MonoBehaviour {
+
+    public int currHeadNum;
+    public Sprite[] heads;
+
+    public int currHeadColorNum;
+    public int[,] colors = new int[4, 4] { {255,255,255,255 },
+                                           {75,75,10,255 },
+                                           {100,10,10,255 },
+                                           {10,100,10,255 },
+                                            };
+
+
+    public void Start()
+    {
+        Debug.Log("Inside start of CharacterCreationMenu");
+        //GameObject.FindWithTag("head").GetComponent<Image>().sprite = Heads[0];
+
+        currHeadColorNum = 0;
+        GameObject.FindWithTag("head").GetComponent<Image>().color = new Color32((byte)colors[0,0],
+                                                                                 (byte)colors[0,1],
+                                                                                 (byte)colors[0,2],
+                                                                                 (byte)colors[0,3]);
+
+    }
+
+    public void cycleBodyPart(string bodyPartAndDirection)
+    {
+        string[] bodyPartAndDir = bodyPartAndDirection.Split();
+        Debug.Log(bodyPartAndDir[0]);
+        Debug.Log(bodyPartAndDir[1]);
+
+        if(bodyPartAndDir[1] == "right")
+        {
+            currHeadNum++;
+        }
+        else
+        {
+            currHeadNum--;
+        }
+
+        if(currHeadNum < 0)
+        {
+            currHeadNum = heads.Length - 1;
+        } else if(currHeadNum > heads.Length -1)
+        {
+            currHeadNum = 0;
+        }
+
+        GameObject.FindWithTag("head").GetComponent<Image>().sprite = heads[currHeadNum];
+
+    }
+
+    public void cycleBodyPartColor(string bodyPartAndDirection)
+    {
+        string[] bodyPartAndDir = bodyPartAndDirection.Split();
+        Debug.Log(bodyPartAndDir[0]);
+        Debug.Log(bodyPartAndDir[1]);
+        Debug.Log(colors.Length);
+        if (bodyPartAndDir[1] == "right")
+        {
+            currHeadColorNum++;
+        }
+        else
+        {
+            currHeadColorNum--;
+        }
+
+        if (currHeadColorNum < 0)
+        {
+            currHeadColorNum = colors.Length/4 - 1;
+        }
+        else if (currHeadColorNum > colors.Length/4 - 1)
+        {
+            currHeadColorNum = 0;
+        }
+
+        GameObject.FindWithTag("head").GetComponent<Image>().color = new Color32((byte)colors[currHeadColorNum, 0],
+                                                                                 (byte)colors[currHeadColorNum, 1],
+                                                                                 (byte)colors[currHeadColorNum, 2],
+                                                                                 (byte)colors[currHeadColorNum, 3]);
+            
+         }
+
+
+    public void cycleBodyPartTEST(string direction)
+    {
+        Debug.Log(direction);
+    }
+
+}
---
+++ TB-RPG/Assets/Scripts/UI/TitleMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleMenuScript : MonoBehaviour {
+
+    //Loads the scene based on argument
+    public void LoadScene(string sceneToLoad)
+    {
+        Debug.Log(sceneToLoad);
+        //Application.LoadLevel(sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
+        
+    }
+}
