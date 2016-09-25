47dfa8cbe6481c8b29e8a7d01aea5851956b42ce
aaronmwilkins <aaronmwilkins323@gmail.com>
2016-09-24 21:53:41 -0400

    We now have basic combat set up
    I mapped an attack button and an item button to the keyboard (1 to
    attack, 2 to use an item). Added background I drew earlier. You will
    need to have the debug log open to see the damage dealt. I'll try to
    clean things up a little bit and maybe add on screen text.

---
+++ TB-RPG/Assets/Combat.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+    public GameObject DemonSkull;
+
+    //We'll use this variable to keep track of whose turn it is
+    public static bool PlayerTurn = true;
+
+    public static int playerHP = 200; 
+    public static int playerAttack = 25;
+
+    public static int enemyHP = 150;
+    public static int enemyAttack = 10;
+
+    public static int potion = 50;
+
+    //Using KeyCode lets you map an action to a key on the keyboard
+    public KeyCode attack;
+    public KeyCode item;  
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //activates when player releases key assigned to variable attack
+        if (Input.GetKeyUp(attack) && Combat.PlayerTurn == true)
+        {
+            Combat.enemyHP -= playerAttack;
+            Debug.Log("EnemyHP: " + enemyHP);
+            Combat.PlayerTurn = false;
+        }
+	    //activates when player releases key assigned to variable item
+        if (Input.GetKeyUp(item) & Combat.PlayerTurn == true)
+        {
+            Combat.playerHP += potion;
+            Debug.Log("Player health: " + playerHP);
+            Combat.PlayerTurn = false;
+        }
+        
+        //enemies turn
+        if (PlayerTurn == false)
+        {
+            if (enemyHP < 50)
+                enemyAttack = 30;
+            else
+                enemyAttack = 10;
+
+            Combat.playerHP -= enemyAttack;
+            Combat.PlayerTurn = true;
+
+            Debug.Log("PlayerHP: " + Combat.playerHP);
+        }
+	}
+}
