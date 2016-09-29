e18ee638807ac29287af34665e473dde343f9319
aaronmwilkins <aaronmwilkins323@gmail.com>
2016-09-28 22:00:21 -0400

    We can now set skills
    I programed the skill menu so that we can adjust the skill values.

--- TB-RPG/Assets/Scripts/UI/SkillSelectionScript.cs
+++ TB-RPG/Assets/Scripts/UI/SkillSelectionScript.cs
@@ -3,13 +3,111 @@ using System.Collections;
 
 public class SkillSelectionScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static int statTotal = 10;
+    public static int strength = 0;
+    public static int intellect = 0;
+    public static int dexterity = 0;
+    public static int cunning = 0;
+    public static int charisma = 0;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 
     public void goToNextScene()
     {
         GameMaster.instance.switchCamera(4);
+    }
+
+    public void StrDec()
+    {
+       if (statTotal < 10 && strength > 0)
+        {
+            statTotal++;
+            strength--;
+            Debug.Log("statTotal:" + statTotal + "strength" + strength);
+        }
+    }
+
+    public void IntDec()
+    {
+        if (statTotal < 10 && intellect > 0)
+        {
+            statTotal++;
+            intellect--;
+        }
+    }
+
+    public void DexDec()
+    {
+        if (statTotal < 10 && dexterity > 0)
+        {
+            statTotal++;
+            dexterity--;
+        }
+    }
+
+    public void CunDec()
+    {
+        if (statTotal < 10 && cunning > 0)
+        {
+            statTotal++;
+            cunning--;
+        }
+    }
+
+    public void CharDec()
+    {
+        if (statTotal < 10 && charisma > 0)
+        {
+            statTotal++;
+            charisma--;
+        }
+    }
+
+    public void StrInc()
+    {
+        if (statTotal > 0)
+        {
+            statTotal--;
+            strength++;
+        }
+    }
+
+    public void IntInc()
+    {
+        if (statTotal > 0)
+        {
+            statTotal--;
+            intellect++;
+        }
+    }
+
+    public void DexInc()
+    {
+        if (statTotal > 0)
+        {
+            statTotal--;
+            dexterity++;
+        }
+    }
+
+    public void CunInc()
+    {
+        if (statTotal > 0)
+        {
+            statTotal--;
+            cunning++;
+        }
+    }
+
+    public void CharInc()
+    {
+        if (statTotal > 0)
+        {
+            statTotal--;
+            charisma++;
+        }
     }
 }
--- TB-RPG/Assets/Scripts/UI/StatChange.cs
+++ TB-RPG/Assets/Scripts/UI/StatChange.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class StatChange : MonoBehaviour {
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +13,35 @@ public class StatChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+	    if (gameObject.name == "Stats")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.statTotal);
+        }
+
+        if (gameObject.name == "Strength")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.strength);
+        }
+
+        if (gameObject.name == "Intellect")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.intellect);
+        }
+
+        if (gameObject.name == "Dexterity")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.dexterity);
+        }
+
+        if (gameObject.name == "Cunning")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.cunning);
+        }
+
+        if (gameObject.name == "Charisma")
+        {
+            GetComponent<TextMesh>().text = Convert.ToString(SkillSelectionScript.charisma);
+        }
+
+    }
 }
