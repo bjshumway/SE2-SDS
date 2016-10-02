c46e633154d891c414609cda12b898e8ca3d18e4
aaronmwilkins <aaronmwilkins323@gmail.com>
2016-10-02 00:01:58 -0400

    Added Health Bars
    I added Health bars for the player and enemy. We need to work on
    creating abilities and implementing them into a new combat script since
    mine is not really the greatest.

---
+++ TB-RPG/Assets/EnemyHP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyHP : MonoBehaviour {
+    public Slider EnemyHp;
+
+	// Use this for initialization
+	void Start () {
+        EnemyHp.maxValue = Combat.enemyHP;
+        EnemyHp.value = Combat.enemyHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        EnemyHp.value = Combat.enemyHP;
+	}
+}
---
+++ TB-RPG/Assets/HeathControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HeathControl : MonoBehaviour {
+    public Slider PlayerHP;
+	// Use this for initialization
+	void Start () {
+        PlayerHP.maxValue = Combat.playerHP;
+        PlayerHP.value = Combat.playerHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerHP.value = Combat.playerHP;
+	}
+}
