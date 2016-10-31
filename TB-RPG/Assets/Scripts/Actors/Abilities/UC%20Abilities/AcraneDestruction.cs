1d968a5679236793e13e62fb1d42ec906457ecaa
Collen Irwin <collenirwin@gmail.com>
2016-10-31 13:34:08 -0400

    Moving to 1 ArcaneDestruction class (rather than 3)

---
+++ TB-RPG/Assets/Scripts/Actors/Abilities/UC%20Abilities/AcraneDestruction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ArcaneDestruction : SingleTargetAbility {
+
+    public GameObject ADSelectionButton = GameObject.Find("ArcaneDestructionSelection");
+
+    public override void showAnimation(Monster m) {
+        //Program animation here
+        //We might have a static class of generic animations that this can refer to
+        //Also each monster will contain a reference to its image, to make things easier
+    }
+
+    public ArcaneDestruction(Actor Owner) : base("Arcane Destruction", "Deals 100% of the selected damage type",
+        "intellect", 1.0m, 75, false, Owner, damageType.fire) {
+        ADSelectionButton.SetActive(false);
+        //value
+        //ADSelectionButton
+    }
+}
