26e98f54c774eb083adee3caeea71cded0fa4aca
Collen Irwin <collenirwin@gmail.com>
2016-11-09 15:04:04 -0500

    Add new project for translation testing
    I don't have access to Unity right now, and I wanted to be able to test my code, so I made a testing project.
    MLH has functionality for translating text using an English file, and a corresponding file in another language
    Use populateDict("otherlanguagefilename") to load everything into the dict.
    Use tr("original text", true) to translate to the other language
    Use tr("original text", false) to translate from the other language to English

---
+++ TranslationTest/TranslationTest/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TranslationTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chkEnglish = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkEnglish
+            // 
+            this.chkEnglish.AutoSize = true;
+            this.chkEnglish.Checked = true;
+            this.chkEnglish.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEnglish.Location = new System.Drawing.Point(13, 13);
+            this.chkEnglish.Name = "chkEnglish";
+            this.chkEnglish.Size = new System.Drawing.Size(60, 17);
+            this.chkEnglish.TabIndex = 0;
+            this.chkEnglish.Text = "English";
+            this.chkEnglish.UseVisualStyleBackColor = true;
+            this.chkEnglish.CheckedChanged += new System.EventHandler(this.chkEnglish_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello world!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "This is some text.";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(13, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 129);
+            this.label3.TabIndex = 3;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkEnglish);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkEnglish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
