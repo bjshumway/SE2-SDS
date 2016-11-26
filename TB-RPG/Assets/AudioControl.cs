d86656e191a8a9fe5395a82c8bde3601d3fe0fb8
aaronmwilkins <aaronmwilkins323@gmail.com>
2016-11-26 12:01:11 -0500

    Started testing sound effects
    Ben+Aaron+Collen

--- TB-RPG/Assets/AudioControl.cs
+++ TB-RPG/Assets/AudioControl.cs
@@ -4,14 +4,34 @@ using System.Collections;
 public class AudioControl : MonoBehaviour {
 
     public AudioSource source;
+    public AudioClip dynamic;
 
     // Use this for initialization
     void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        AudioClip ac = (AudioClip) Resources.Load("Audio/ice_spell");
+        /*source.PlayOneShot(ac);
+        // ac = (AudioClip)Resources.Load("Audio/door_open");
+
+        //source.PlayOneShot(ac);
+        ac = (AudioClip)Resources.Load("Audio/fire_spell");
+        source.volume = 1;
+        source.PlayOneShot(ac);
+        */
+
+        var aSourceGO = Resources.Load("AudioControllerSoundEffect");
+        GameObject aSource = (GameObject) Instantiate(aSourceGO);
+        aSource.GetComponent<AudioControl>().dynamic = (AudioClip)Resources.Load("Audio/door_open");//(ac);
+        aSource.GetComponent<AudioControl>().playSound(ac);
+
+    }
+
+    void playSound(AudioClip ac)
+    {
+        source.PlayOneShot(ac);
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 
 
