6dc3e737710f851c9f4973bee7582f292f953419
aaronmwilkins <aaronmwilkins323@gmail.com>
2016-11-29 18:04:14 -0500

    Added transitions between orca animations got rid of mipmaps in newly added sprites

---
+++ TB-RPG/Assets/BGM.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour {
+
+  public AudioSource source1;
+  public AudioSource source2;
+  public AudioSource source3;
+  public AudioSource source4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
